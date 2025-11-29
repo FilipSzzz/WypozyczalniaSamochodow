323e99fb11eb46c58dbd23dc1b8283927aa6d939
FilipSzzz <121720963+FilipSzzz@users.noreply.github.com>
2025-11-29 14:36:09 +0100

    Update RentalService.java
    zjazd 5

--- src/main/java/wypozyczalniasamochodw/RentalService.java
+++ src/main/java/wypozyczalniasamochodw/RentalService.java
@@ -15,19 +15,23 @@ public class RentalService {
     }
 
     public void wypozyczenie(String model, LocalDate dateFrom, LocalDate dateTo, int clientId){
-        Car car = this.carStorage.getCarByModel(model);
 
-        if (car != null && dostepnosc(car.getVin(), dateFrom, dateTo)){
-            Rental rental = new Rental(dateFrom, dateTo, clientId, car.getVin());
-            rentalStorage.addRental(rental);
-            System.out.println("Samochod wypozyczony pomyslnie: " + rental);
-        } else {
-            System.out.println("Samochod niedostepny w wybranym terminie");
+    }
+
+    public boolean dostepnosc(String model, LocalDate dateFrom, LocalDate dateTo) {
+        if (carStorage.getCarByModel(model)==null){
+            return false;
         }
+
+
+
+    }
+    public boolean isModelAvailableInDate(int vin, LocalDate dateFrom, LocalDate dateTo){
+        if(carStorage.getCarByVin(vin)){
+            return
+        }
+
     }
 
-    public boolean dostepnosc(int vin, LocalDate dateFrom, LocalDate dateTo) {
-        return rentalStorage.isAvailable(vin, dateFrom, dateTo);
-    }
 }
 
